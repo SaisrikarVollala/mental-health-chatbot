238c7ea75824f16da661f2698564ae6753c5b807
Vrittigyl <vritti.gyl@gmail.com>
2026-07-15 13:38:42 +0530

    Add cause and intent model

---
+++ intent.py
@@ -0,0 +1,13 @@
+import spacy
+nlp = spacy.load("en_core_web_trf")  
+
+def extract_action_object(text):
+    doc = nlp(text)
+    for token in doc:
+        if token.dep_ == "dobj":
+            action = token.head.lemma_        # verb governing the object
+            obj = token.text                  # or expand to full noun phrase via token.subtree
+            return action, obj
+    return None, None
+
+print(extract_action_object("i want to book a flight for hyderabad to chandigarh"))
